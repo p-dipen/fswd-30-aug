7cdf43d527d3b65451d949a5bbbd6aba0fc38e85
Dipen Parihar <parihar.dipen@gmail.com>
2025-08-01 19:45:10 -0400

    Break-continue-loops

---
+++ Break-continue-loops/lesson03/index.js
@@ -0,0 +1,8 @@
+// Part1: loopCounter = 1 => the initial is 1 for the loop counter variable "loopCounter"
+// Part2: loopCounter <= 10 => keep looping while the loopCounter is less than/equal to 10
+// Part3: loopCounter++ => to increment the loopCounter by 1 in each iteration
+for (var loopCounter = 1; loopCounter <= 10; loopCounter++) {
+  // the for loop code goes here
+  // This code/message will be printed 10 times!
+  console.log(5 * loopCounter);
+}
